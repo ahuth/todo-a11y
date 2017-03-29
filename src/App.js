fca83034e0b5ce75e7621dc816ed5c0693180d45
Andrew <ahuth@mavenlink.com>
2017-03-28 21:56:17 -0700

    Add non-functional header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import Header from './header'
 
 export default function () {
-  return <span>Hello World!</span>
+  return (
+    <div className="todoapp">
+      <Header />
+    </div>
+  )
 }
