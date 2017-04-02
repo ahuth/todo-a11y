0c8e2a700151f112696ba11bdb57aa9845c2ddcb
Andrew <ahuth@mavenlink.com>
2017-04-02 08:54:00 -0700

    Wrap long line

--- src/header.js
+++ src/header.js
@@ -4,7 +4,13 @@ export default function ({value, onChange, onKeyPress}) {
   return (
     <header className="header">
       <h1>todos</h1>
-      <input className="new-todo" placeholder="What needs to be done?" value={value} onChange={onChange} onKeyPress={onKeyPress}></input>
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        value={value}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
     </header>
   )
 }
