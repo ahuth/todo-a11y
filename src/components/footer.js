80d532981f5e2f40d129b62ae6254da11b44ce2e
Andrew <ahuth@mavenlink.com>
2017-04-02 11:14:37 -0700

    Extract function to pluralize a word

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,11 @@
 import React from "react"
+import pluralize from "../utils/pluralize"
 
 export default function ({todos, clearCompleted}) {
   const uncompletedCount = todos.filter(todo => !todo.completed).length
   return (
     <footer className="footer">
-      <span className="todo-count">{uncompletedCount} item{uncompletedCount !== 1 ? "s" : ""} left</span>
+      <span className="todo-count">{uncompletedCount} {pluralize("item", uncompletedCount)} left</span>
       {uncompletedCount < todos.length &&
         <button className="clear-completed" onClick={clearCompleted}>Clear completed</button>
       }
---
+++ src/utils/pluralize.js
@@ -0,0 +1,6 @@
+export default function (word, number) {
+  if (number === 1) {
+    return word
+  }
+  return word + "s"
+}
