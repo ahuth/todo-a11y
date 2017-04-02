1923a1df2954ee5b536715606b0cfbe61c287eed
Andrew <ahuth@mavenlink.com>
2017-04-02 12:50:54 -0700

    Extract <HeaderContainer>

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,18 +1,15 @@
 import React, {Component} from "react"
-import Header from "../components/header"
+import HeaderContainer from "../containers/header"
 import Footer from "../components/footer"
 import Todos from "../components/todos"
 import store from "../store";
-import {ENTER} from "../utils/key-codes"
-import {toggleAll, clearCompleted, changeHeader, addTodo, destroyTodo, toggleTodo} from "../actions"
+import {toggleAll, clearCompleted, destroyTodo, toggleTodo} from "../actions"
 
 export default class extends Component {
   constructor(props) {
     super(props)
     this.state = store.getState()
     this.onChange = this.onChange.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.handleHeaderKeyPress = this.handleHeaderKeyPress.bind(this)
     this.handleToggleTodo = this.handleToggleTodo.bind(this)
     this.handleDestroyTodo = this.handleDestroyTodo.bind(this)
     this.handleToggleAll = this.handleToggleAll.bind(this)
@@ -29,16 +26,6 @@ export default class extends Component {
 
   onChange() {
     this.setState(store.getState())
-  }
-
-  handleChange(event) {
-    store.dispatch(changeHeader(event.target.value))
-  }
-
-  handleHeaderKeyPress(event) {
-    if (event.charCode === ENTER) {
-      store.dispatch(addTodo(event.target.value))
-    }
   }
 
   handleToggleTodo(id) {
@@ -60,7 +47,7 @@ export default class extends Component {
   render() {
     return (
       <div className="todoapp">
-        <Header value={this.state.value} onChange={this.handleChange} onKeyPress={this.handleHeaderKeyPress} />
+        <HeaderContainer value={this.state.value} dispatch={store.dispatch} />
         {this.state.todos.length > 0 &&
           <div>
             <Todos todos={this.state.todos} toggleTodo={this.handleToggleTodo} destroyTodo={this.handleDestroyTodo} toggleAll={this.handleToggleAll} />
