fe802d84aa10008e4939a6b7503fadb144b0dfaf
Andrew <ahuth@mavenlink.com>
2017-04-02 11:15:46 -0700

    Extract variable

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,10 +3,11 @@ import pluralize from "../utils/pluralize"
 
 export default function ({todos, clearCompleted}) {
   const uncompletedCount = todos.filter(todo => !todo.completed).length
+  const areCompleted = uncompletedCount < todos.length
   return (
     <footer className="footer">
       <span className="todo-count">{uncompletedCount} {pluralize("item", uncompletedCount)} left</span>
-      {uncompletedCount < todos.length &&
+      {areCompleted &&
         <button className="clear-completed" onClick={clearCompleted}>Clear completed</button>
       }
     </footer>
