c3169934511b334aaa4d61111e3837556804039d
Andrew <ahuth@mavenlink.com>
2017-04-02 20:27:18 -0700

    Add tests for the <Todos> component

---
+++ src/components/__tests__/todos.test.js
@@ -0,0 +1,61 @@
+import React from "react"
+import Todos from "components/todos"
+import {shallow} from "enzyme"
+
+const render = (options = {}) => {
+  const func = () => {}
+  const defaults = {todos: [], onDestroyClick: func, onToggleClick: func, onToggleAllClick: func}
+  const props = Object.assign(defaults, options)
+  return shallow(
+    <Todos {...props} />
+  )
+}
+
+describe("components todos", () => {
+  describe("the toggle-all checkbox", () => {
+    const checkbox = component => component.find(".toggle-all")
+
+    describe("when some todos are not completed", () => {
+      beforeEach(function () {
+        this.component = render({
+          todos: [{id: 1, title: "one", completed: false}]
+        })
+      })
+
+      it("is unchecked", function () {
+        expect(checkbox(this.component).prop("checked")).toBe(false)
+      })
+    })
+
+    describe("when all todos are completed", () => {
+      beforeEach(function () {
+        this.component = render({
+          todos: [{id: 1, title: "one", completed: true}]
+        })
+      })
+
+      it("is checked", function () {
+        expect(checkbox(this.component).prop("checked")).toBe(true)
+      })
+    })
+  })
+
+  describe("the todos list", () => {
+    const list = component => component.find(".todo-list")
+
+    beforeEach(function () {
+      this.component = render({
+        todos: [
+          {id: 1, title: "one", completed: false},
+          {id: 2, title: "two", completed: false},
+        ]
+      })
+    })
+
+    it("contains all of the todos", function () {
+      const items = list(this.component).find("TodoItem")
+      const titles = items.map(item => item.prop("todo").title)
+      expect(titles).toEqual(["one", "two"])
+    })
+  })
+})
