1ba1e4f0f8174f9c565d9f1ed5ce18cd7f6e19b2
Andrew <ahuth@mavenlink.com>
2017-04-02 18:40:14 -0700

    Add tests for `pluralize`

---
+++ src/utils/__tests__/pluralize.test.js
@@ -0,0 +1,15 @@
+import pluralize from "../pluralize"
+
+describe("pluralize", () => {
+  it("pluralizes 0", () => {
+    expect(pluralize("foobar", 0)).toEqual("foobars")
+  })
+
+  it("does not pluralize 1", () => {
+    expect(pluralize("foobar", 1)).toEqual("foobar")
+  })
+
+  it("pluralizes anything over 2", () => {
+    expect(pluralize("foobar", 2)).toEqual("foobars")
+  })
+})
