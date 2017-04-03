fe43239ffe7d71cba7c6d624adbc76c59c0ecfc4
Andrew <ahuth@mavenlink.com>
2017-04-02 20:12:40 -0700

    Add tests for the <TodoItem> component

---
+++ src/components/__tests__/todo-item.test.js
@@ -0,0 +1,30 @@
+import React from "react"
+import TodoItem from "components/todo-item"
+import {shallow} from "enzyme"
+
+const render = (options = {}) => {
+  const todo = {id: 666, title: "Get coffee", completed: true}
+  const defaults = {todo: todo, toggleTodo: () => {}, destroyTodo: () => {}}
+  const props = Object.assign(defaults, options)
+  return shallow(
+    <TodoItem {...props} />
+  )
+}
+
+describe("components todo-item", () => {
+  beforeEach(function () {
+    this.component = render()
+  })
+
+  it("renders correctly", function () {
+    expect(this.component.matchesElement(
+      <li>
+        <div>
+          <input type="checkbox" checked={true} />
+          <label>Get coffee</label>
+          <button></button>
+        </div>
+      </li>
+    )).toBe(true)
+  })
+})
