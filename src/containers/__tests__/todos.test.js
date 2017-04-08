4c6f3eb260fa1e18625940fc912e0b401bde9cd7
Andrew <ahuth@mavenlink.com>
2017-04-08 11:46:09 -0700

    Test the <Todos> container

---
+++ src/containers/__tests__/todos.test.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Todos from "containers/todos"
+import {createMockStore} from "redux-test-utils"
+import {shallow} from "enzyme"
+
+const render = (state = {}) => {
+  const store = createMockStore(state)
+  return shallow(<Todos store={store} />)
+}
+
+describe("containers <Todos />", () => {
+  const state = {todos: [{id: 0}, {id: 1}, {id: 2}]}
+
+  beforeEach(function () {
+    this.component = render(state)
+  })
+
+  it("has the right todos", function () {
+    expect(this.component.prop("todos")).toEqual(state.todos)
+  })
+})
