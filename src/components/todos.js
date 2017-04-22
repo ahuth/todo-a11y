602ff446d0728ded0d26b289ff25e7515b878a2c
Andrew <ahuth@mavenlink.com>
2017-04-22 09:58:12 -0700

    Label the toggle all button

--- src/components/todos.js
+++ src/components/todos.js
@@ -4,12 +4,13 @@ import TodoItem from "./todo-item"
 export default function Todos({todos, onToggleClick, onDestroyClick, onToggleAllClick}) {
   return (
     <section className="main">
-      <input className="toggle-all hand" type="checkbox" checked={todos.every(todo => todo.completed)} onClick={onToggleAllClick} />
+      <input className="toggle-all hand" type="checkbox" checked={todos.every(todo => todo.completed)} onClick={onToggleAllClick} aria-labelledby="toggle-all-label" />
       <ul className="todo-list">
         {todos.map((todo) => {
           return <TodoItem key={todo.id} todo={todo} toggleTodo={onToggleClick} destroyTodo={onDestroyClick} />
         })}
       </ul>
+      <span hidden id="toggle-all-label">Toggle All</span>
     </section>
   )
 }
